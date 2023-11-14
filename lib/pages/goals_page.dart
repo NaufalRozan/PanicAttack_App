af57f213fd731b13988da5772d082f47e94766ad
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2023-11-14 16:44:18 +0700

    Menambahkan gambar di goals page

--- lib/pages/goals_page.dart
+++ lib/pages/goals_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:panicattack_app/constans.dart';
 
 class GoalsPage extends StatefulWidget {
-  const GoalsPage({ Key? key }) : super(key: key);
+  const GoalsPage({Key? key}) : super(key: key);
 
   @override
   _GoalsPageState createState() => _GoalsPageState();
@@ -11,7 +12,53 @@ class _GoalsPageState extends State<GoalsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 60),
+                ),
+                // Gambar dengan border di atas dan bawah
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(width: 2.0, color: primaryButtonColor),
+                      bottom: BorderSide(width: 2.0, color: primaryButtonColor),
+                    ),
+                  ),
+                  child: Image.asset(
+                    'lib/assets/images/Stretching exercises-pana.png',
+                    width: double
+                        .infinity, // Sesuaikan lebar gambar sesuai kebutuhan
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                // Widget lainnya dapat ditambahkan di sini
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.3,
+              height: MediaQuery.of(context).size.height * 0.1,
+              decoration: BoxDecoration(
+                color: primaryButtonColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text(
+                  'Goals',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: MediaQuery.of(context).size.width * 0.07, // Sesuaikan ukuran font sesuai kebutuhan
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
