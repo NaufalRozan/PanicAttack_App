84fce8e51ae354a02c9220f7df4acf582bab1d20
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2023-11-15 21:50:13 +0700

    Menambahkan fungsi button

--- lib/pages/goals_day1_page.dart
+++ lib/pages/goals_day1_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:panicattack_app/constans.dart';
+import 'package:panicattack_app/core.dart';
 
 class GoalsDay1Page extends StatefulWidget {
   const GoalsDay1Page({Key? key}) : super(key: key);
@@ -66,217 +67,231 @@ class _GoalsDay1PageState extends State<GoalsDay1Page> {
                       top: 20,
                     ),
                   ),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.18,
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    decoration: BoxDecoration(
-                      color: whiteColor,
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                        width: 1,
-                        color: greyColor,
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.18,
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      decoration: BoxDecoration(
+                        color: whiteColor,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          width: 1,
+                          color: greyColor,
+                        ),
                       ),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: 15,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: 15,
+                            ),
                           ),
-                        ),
-                        Image.asset(
-                          'lib/assets/images/Breathing exercise-amico.png',
-                          width: MediaQuery.of(context).size.height *
-                              0.13, // Set the width as needed
-                          fit: BoxFit
-                              .contain, // Adjust the fit based on your preference
-                        ),
-                        SizedBox(width: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Tarik Nafas Panjang',
-                              style: textStyle.copyWith(
-                                  fontSize:
-                                      MediaQuery.of(context).size.height * 0.03,
-                                  fontWeight: bold),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              '00:30',
-                              style: greyTextStyle.copyWith(
-                                  fontSize: MediaQuery.of(context).size.height *
-                                      0.03),
-                            ),
-                          ],
-                        ),
-                      ],
+                          Image.asset(
+                            'lib/assets/images/Breathing exercise-amico.png',
+                            width: MediaQuery.of(context).size.height *
+                                0.13, // Set the width as needed
+                            fit: BoxFit
+                                .contain, // Adjust the fit based on your preference
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Tarik Nafas Panjang',
+                                style: textStyle.copyWith(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height *
+                                            0.03,
+                                    fontWeight: bold),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '00:30',
+                                style: greyTextStyle.copyWith(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height *
+                                            0.03),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   SizedBox(
                     height: 20,
                   ),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.18,
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    decoration: BoxDecoration(
-                      color: whiteColor,
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                        width: 1,
-                        color: greyColor,
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.18,
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      decoration: BoxDecoration(
+                        color: whiteColor,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          width: 1,
+                          color: greyColor,
+                        ),
                       ),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: 15,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: 15,
+                            ),
                           ),
-                        ),
-                        Image.asset(
-                          'lib/assets/images/Pilates-amico.png',
-                          width: MediaQuery.of(context).size.height *
-                              0.13, // Set the width as needed
-                          fit: BoxFit
-                              .contain, // Adjust the fit based on your preference
-                        ),
-                        SizedBox(width: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Relaksasi',
-                              style: textStyle.copyWith(
-                                  fontSize:
-                                      MediaQuery.of(context).size.height * 0.03,
-                                  fontWeight: bold),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              '01:00',
-                              style: greyTextStyle.copyWith(
-                                  fontSize: MediaQuery.of(context).size.height *
-                                      0.03),
-                            ),
-                          ],
-                        ),
-                      ],
+                          Image.asset(
+                            'lib/assets/images/Pilates-amico.png',
+                            width: MediaQuery.of(context).size.height *
+                                0.13, // Set the width as needed
+                            fit: BoxFit
+                                .contain, // Adjust the fit based on your preference
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Relaksasi',
+                                style: textStyle.copyWith(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height * 0.03,
+                                    fontWeight: bold),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '01:00',
+                                style: greyTextStyle.copyWith(
+                                    fontSize: MediaQuery.of(context).size.height *
+                                        0.03),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   SizedBox(
                     height: 20,
                   ),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.18,
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    decoration: BoxDecoration(
-                      color: whiteColor,
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                        width: 1,
-                        color: greyColor,
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.18,
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      decoration: BoxDecoration(
+                        color: whiteColor,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          width: 1,
+                          color: greyColor,
+                        ),
                       ),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: 15,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: 15,
+                            ),
                           ),
-                        ),
-                        Image.asset(
-                          'lib/assets/images/Imagination-amico.png',
-                          width: MediaQuery.of(context).size.height *
-                              0.13, // Set the width as needed
-                          fit: BoxFit
-                              .contain, // Adjust the fit based on your preference
-                        ),
-                        SizedBox(width: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Visualisasi',
-                              style: textStyle.copyWith(
-                                  fontSize:
-                                      MediaQuery.of(context).size.height * 0.03,
-                                  fontWeight: bold),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              '00:45',
-                              style: greyTextStyle.copyWith(
-                                  fontSize: MediaQuery.of(context).size.height *
-                                      0.03),
-                            ),
-                          ],
-                        ),
-                      ],
+                          Image.asset(
+                            'lib/assets/images/Imagination-amico.png',
+                            width: MediaQuery.of(context).size.height *
+                                0.13, // Set the width as needed
+                            fit: BoxFit
+                                .contain, // Adjust the fit based on your preference
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Visualisasi',
+                                style: textStyle.copyWith(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height * 0.03,
+                                    fontWeight: bold),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '00:45',
+                                style: greyTextStyle.copyWith(
+                                    fontSize: MediaQuery.of(context).size.height *
+                                        0.03),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   SizedBox(
                     height: 20,
                   ),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.18,
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    decoration: BoxDecoration(
-                      color: whiteColor,
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                        width: 1,
-                        color: greyColor,
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.18,
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      decoration: BoxDecoration(
+                        color: whiteColor,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          width: 1,
+                          color: greyColor,
+                        ),
                       ),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: 15,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: 15,
+                            ),
                           ),
-                        ),
-                        Image.asset(
-                          'lib/assets/images/Mindfulness-amico.png',
-                          width: MediaQuery.of(context).size.height *
-                              0.13, // Set the width as needed
-                          fit: BoxFit
-                              .contain, // Adjust the fit based on your preference
-                        ),
-                        SizedBox(width: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Meditasi',
-                              style: textStyle.copyWith(
-                                  fontSize:
-                                      MediaQuery.of(context).size.height * 0.03,
-                                  fontWeight: bold),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              '00:30',
-                              style: greyTextStyle.copyWith(
-                                  fontSize: MediaQuery.of(context).size.height *
-                                      0.03),
-                            ),
-                          ],
-                        ),
-                      ],
+                          Image.asset(
+                            'lib/assets/images/Mindfulness-amico.png',
+                            width: MediaQuery.of(context).size.height *
+                                0.13, // Set the width as needed
+                            fit: BoxFit
+                                .contain, // Adjust the fit based on your preference
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Meditasi',
+                                style: textStyle.copyWith(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height * 0.03,
+                                    fontWeight: bold),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '00:30',
+                                style: greyTextStyle.copyWith(
+                                    fontSize: MediaQuery.of(context).size.height *
+                                        0.03),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
