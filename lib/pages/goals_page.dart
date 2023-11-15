f30fb33d7bf9a6cbdbc397027a7491e2c129f0fa
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2023-11-15 15:07:30 +0700

    Menambahkan box-box pemilihan

--- lib/pages/goals_page.dart
+++ lib/pages/goals_page.dart
@@ -13,57 +13,112 @@ class _GoalsPageState extends State<GoalsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 60),
-                ),
-                // Gambar dengan border di atas dan bawah
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border(
-                      top: BorderSide(width: 2.0, color: primaryButtonColor),
-                      bottom: BorderSide(width: 2.0, color: primaryButtonColor),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 60),
+                      ),
+                      // Gambar dengan border di atas dan bawah
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            top: BorderSide(
+                                width: 2.0, color: primaryButtonColor),
+                            bottom: BorderSide(
+                                width: 2.0, color: primaryButtonColor),
+                          ),
+                        ),
+                        child: Image.asset(
+                          'lib/assets/images/Stretching exercises-pana.png',
+                          width: double
+                              .infinity, // Sesuaikan lebar gambar sesuai kebutuhan
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      // Widget lainnya dapat ditambahkan di sini
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      decoration: BoxDecoration(
+                        color: primaryButtonColor,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(12),
+                          bottomRight: Radius.circular(12),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Goals',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: MediaQuery.of(context).size.width *
+                                0.07, // Sesuaikan ukuran font sesuai kebutuhan
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ),
                   ),
-                  child: Image.asset(
-                    'lib/assets/images/Stretching exercises-pana.png',
-                    width: double
-                        .infinity, // Sesuaikan lebar gambar sesuai kebutuhan
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                // Widget lainnya dapat ditambahkan di sini
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.3,
-                height: MediaQuery.of(context).size.height * 0.07,
-                decoration: BoxDecoration(
-                  color: primaryButtonColor,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(12),
-                    bottomRight: Radius.circular(12),
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    'Goals',
-                    style: whiteTextStyle.copyWith(
-                      fontSize: MediaQuery.of(context).size.width *
-                          0.07, // Sesuaikan ukuran font sesuai kebutuhan
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
+                ],
               ),
-            ),
-          ],
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 50,),
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.15,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                        color: primaryButtonColor,
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.15,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                        color: primaryButtonColor,
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.15,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                        color: primaryButtonColor,
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.15,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                        color: primaryButtonColor,
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
